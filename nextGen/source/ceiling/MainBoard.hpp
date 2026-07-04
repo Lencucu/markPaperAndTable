db4eff7299fdf57a5807e6596f4f99e95a5cf10b
Lencucu He <lencucu@aliyun.com>
2026-07-04 15:56:48 +0800

    nextGen in building...

---
+++ nextGen/source/ceiling/MainBoard.hpp
@@ -0,0 +1,60 @@
+#ifndef MAINBOARD_HPP
+#define MAINBOARD_HPP
+
+
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QSizePolicy>
+
+
+
+
+// position
+// size
+// style
+// base layout
+
+
+
+
+
+
+
+
+class MainBoard:public QWidget{
+	QVBoxLayout layout{this};
+	QWidget* w1_p=nullptr;
+	QWidget* w2_p=nullptr;
+public:
+	MainBoard(QWidget* w1_p,QWidget* w2_p):w1_p(w1_p),w2_p(w2_p)
+	{	auto& w1=*w1_p;
+		auto& w2=*w2_p;
+
+		w1.setMinimumSize(200,50);
+		w1.setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Fixed);
+		w2.setMinimumSize(200,100);
+		w2.setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
+
+		layout.setSpacing(layout.contentsMargins().left());
+		layout.addWidget(&w1);
+		layout.addWidget(&w2);
+		setObjectName("main");
+		w1.setObjectName("sub1");
+		w2.setObjectName("sub2");
+
+		setStyleSheet(R"(
+			#main{background-color: #D0E8D8;}
+			#sub1{background-color: #D3F1E1;}
+			#sub2{background-color: #D3F1E1;}
+		)");
+	}
+};
+
+
+
+
+
+
+
+
+#endif // MAINBOARD_HPP
---
+++ nextGen/source/main.cpp
@@ -0,0 +1,75 @@
+#include <ceiling/MainBoard.hpp>
+
+#include <QApplication>
+#include <QSystemTrayIcon>
+#include <QMenu>
+#include <QAction>
+#include <QHotkey>
+
+
+int program(int argc, char *argv[])
+{   QApplication a(argc, argv);
+
+
+	QSystemTrayIcon *tray = new QSystemTrayIcon(&a);// 托盘图标
+		tray->setIcon(QIcon(QCoreApplication::applicationDirPath()+"/logo.png"));   // 换成你的图标
+		tray->setToolTip("mpt");
+
+		QMenu menu;// 托盘菜单
+			QAction *quitAct = menu.addAction("退出");
+			QObject::connect(quitAct, &QAction::triggered, &a, &QApplication::quit);
+		tray->setContextMenu(&menu);
+
+
+	MainBoard mainboard{new QWidget,new QWidget};
+		QHotkey hotkey(QKeySequence("Ctrl+Shift+Alt+Z"), true, &a); //The hotkey will be automatically registered//    qDebug() << "Is registered:" << hotkey.isRegistered();
+		QObject::connect(&hotkey, &QHotkey::activated, qApp, [&](){
+			if (mainboard.isVisible()) {
+				if(mainboard.isActiveWindow())
+					mainboard.hide();
+				else
+					mainboard.activateWindow();
+			} else {
+				// mainboard.effect->setOpacity(1);
+				mainboard.show();
+				mainboard.activateWindow();
+			}
+		});
+	QObject::connect(tray, &QSystemTrayIcon::activated, [&](QSystemTrayIcon::ActivationReason reason){
+		if (reason == QSystemTrayIcon::Trigger) { 
+			// 普通点击（单击）处理
+			// qDebug() << "托盘图标被点击";
+			if (mainboard.isVisible()) {
+				if(!mainboard.isActiveWindow())
+					mainboard.activateWindow();
+			} else {
+				// mainboard.effect->setOpacity(1);
+				mainboard.show();
+				mainboard.activateWindow();
+			}
+		}
+		// 如果需要，也可以处理双击：
+		// else if (reason == QSystemTrayIcon::DoubleClick) { ... }
+	});
+	mainboard.show();
+	tray->show();
+
+
+	return a.exec();
+}
+
+
+
+
+#include <windows.h>
+
+
+int main(int argc, char *argv[])
+{	HANDLE hMutex = CreateMutexA(NULL, TRUE, "Global\\MyUniqueAppName");
+	if (GetLastError() == ERROR_ALREADY_EXISTS) return 0; // 已有实例
+
+	program(argc, argv);// 主程序逻辑
+
+	ReleaseMutex(hMutex);
+	CloseHandle(hMutex);
+}
